cbb450cdec6f480276d0436142c8bd0f20650f67
shattikbandyopadhyaa <shattik.bandyopadhyaa@orbitax.com>
2025-01-30 18:55:00 +0600

    removed swagger setup

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e212669308b664e3c520b015666568ffeb5b2c30")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d643399b2e3070f43b9b6b8f077ea231722ff9f2")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
