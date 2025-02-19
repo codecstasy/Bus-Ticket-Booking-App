f510c58a236115220ab73f8225e8e5c8f60f0eba
shattikbandyopadhyaa <shattik.bandyopadhyaa@orbitax.com>
2025-02-19 18:35:45 +0600

    client side bus data work started but not corrected yet

--- client/src/app/available-trips/trip/trip.component.ts
+++ client/src/app/available-trips/trip/trip.component.ts
@@ -1,7 +1,8 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Trip } from '../../models/trip.model';
-import { Router, RouterOutlet } from '@angular/router';
+import { Router } from '@angular/router';
 import { Bus } from '../../models/bus.model';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-trip',
@@ -10,17 +11,19 @@ import { Bus } from '../../models/bus.model';
   templateUrl: './trip.component.html',
   styleUrl: './trip.component.css'
 })
-export class TripComponent {
-  constructor(private router: Router) {}
+export class TripComponent implements OnInit{
+  constructor(private router: Router, private http: HttpClient) {}
+  bus !: Bus;
+  private apiUrl = 'http://localhost:5201/api/busdata/get-bus-data';
+  busId !: number;
+
+  ngOnInit() {
+    this.busId = 2;
+    this.http.get<Bus>(`${this.apiUrl}/${this.busId}`).subscribe();
+  }
 
   @Input()
   trip!: Trip;
-
-  bus!: Bus;
-
-  selectBus(): void {
-    
-  }
 
   navigateToSelectSeats() {
     this.router.navigate(['/select-seats'])
